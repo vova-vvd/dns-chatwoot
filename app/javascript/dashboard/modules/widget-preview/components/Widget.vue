d029b5a7601c9f1acf8ee06a791d9bb7d3f733d5
vova <vova4kin@gmail.com>
2026-02-14 19:08:51 +0000

    translations, branding

--- app/javascript/dashboard/modules/widget-preview/components/Widget.vue
+++ app/javascript/dashboard/modules/widget-preview/components/Widget.vue
@@ -154,23 +154,10 @@ export default {
           :config="getWidgetConfig"
         />
         <WidgetFooter :config="getWidgetConfig" />
-        <div class="py-2.5 flex justify-center">
-          <a
-            class="items-center gap-0.5 text-n-slate-11 cursor-pointer flex filter grayscale opacity-90 hover:grayscale-0 hover:opacity-100 text-xxs"
-          >
-            <img
-              class="max-w-2.5 max-h-2.5"
-              :src="globalConfig.logoThumbnail"
-            />
-            <span>
-              {{
-                replaceInstallationName(
-                  $t('INBOX_MGMT.WIDGET_BUILDER.BRANDING_TEXT')
-                )
-              }}
-            </span>
-          </a>
-        </div>
+        <div
+          class="py-2.5 flex justify-center"
+          :data-none="globalConfig.logoThumbnail"
+        />
       </div>
     </div>
     <div class="flex mt-4 w-[320px]" :style="getBubblePositionStyle">
